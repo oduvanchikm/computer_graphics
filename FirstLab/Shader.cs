5d9f028c19963a6eae958752c741f48a0fb16820
Mira <141563756+oduvanchikm@users.noreply.github.com>
2024-10-16 01:25:39 +0300

    o

---
+++ FirstLab/Shader.cs
@@ -0,0 +1,41 @@
+namespace FirstLab;
+
+using System;
+using System.IO;
+using OpenTK.Graphics.OpenGL4;
+
+public class Shader : IDisposable
+{
+    public int Handle { get; private set; }
+    
+    public Shader(string vertexShaderSource, string fragmentShaderSource)
+    {
+        int vertexShader = GL.CreateShader(ShaderType.VertexShader);
+        GL.ShaderSource(vertexShader, vertexShaderSource);
+        GL.CompileShader(vertexShader);
+
+        int fragmentShader = GL.CreateShader(ShaderType.FragmentShader);
+        GL.ShaderSource(fragmentShader, fragmentShaderSource);
+        GL.CompileShader(fragmentShader);
+
+        Handle = GL.CreateProgram();
+        GL.AttachShader(Handle, vertexShader);
+        GL.AttachShader(Handle, fragmentShader);
+        GL.LinkProgram(Handle);
+
+        GL.DetachShader(Handle, vertexShader);
+        GL.DetachShader(Handle, fragmentShader);
+        GL.DeleteShader(vertexShader);
+        GL.DeleteShader(fragmentShader);
+    }
+
+    public void Use()
+    {
+        GL.UseProgram(Handle);
+    }
+
+    public void Dispose()
+    {
+        GL.DeleteProgram(Handle);
+    }
+}
